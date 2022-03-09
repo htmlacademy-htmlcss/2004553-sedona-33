ac11822d1f585ec93aa50e2592ecef76ba13527c
Keks <keks@htmlacademy.ru>
2022-03-09 11:48:26 +0300

    :heavy_check_mark: Сборка #9

--- 9/gulpfile.js
+++ 9/gulpfile.js
@@ -8,6 +8,7 @@ const chalk = require('chalk');
 const fetch = require('node-fetch');
 const { HtmlValidate } = require('html-validate');
 const gulpIf = require('gulp-if');
+
 let firstRun = true;
 const validateHtml = new HtmlValidate();
 const SeverityLevel = {
@@ -27,10 +28,12 @@ const Severity = {
 const W3C_TIMEOUT = 1000;
 const isDev = process.env.NODE_ENV === 'development';
 const getPage = (tree) => tree.options.from.replace(/^.*source(\\+|\/+)(.*)\.html$/, '$2');
+
 const buildHTML = () => src(['source/**/*.html', '!source/**/_*.html'])
   .pipe(require('gulp-posthtml')([
     (() => (tree) => {
       nunjucks.configure('source', { autoescape: false });
+
       return parser(nunjucks.renderString(render(tree), {
         isDev,
         page: getPage(tree)
@@ -44,8 +47,10 @@ const buildHTML = () => src(['source/**/*.html', '!source/**/_*.html'])
       setTimeout(() => {
         controller.abort();
       }, W3C_TIMEOUT);
+
       try {
         // Онлайн-валидатор HTML
+
         const validRes = await fetch('https://validator.nu/?out=json', {
           body: html,
           headers: { 'Content-Type': 'text/html' },
@@ -57,10 +62,12 @@ const buildHTML = () => src(['source/**/*.html', '!source/**/_*.html'])
           const { logOutput, title } = Severity[SeverityLevel[type]];
           const prefix = `\n[${chalk.cyan('Validate HTML Online')}] ${page} (${lastLine}:${firstColumn + 1})`;
           const selectorMsg = ` ${chalk.cyan(extract)}`;
+
           output += `${prefix}${selectorMsg}:\n${logOutput.underline(title)}: ${logOutput(message)}\n`;
         });
       } catch (err) {
         // Оффлайн-валидатор HTML
+
         const report = validateHtml.validateString(html, {
           extends: [
             'html-validate:recommended',
@@ -68,9 +75,10 @@ const buildHTML = () => src(['source/**/*.html', '!source/**/_*.html'])
           ],
           rules: {
             'attribute-boolean-style': 'off',
-            'no-trailing-whitespace': 'off',
             'input-missing-label': 'off',
-            'require-sri': 'off'
+            'no-trailing-whitespace': 'off',
+            'require-sri': 'off',
+            'svg-focusable': 'off'
           }
         });
         report.results.forEach(({ messages }) => {
@@ -79,6 +87,7 @@ const buildHTML = () => src(['source/**/*.html', '!source/**/_*.html'])
               const { logOutput, title } = Severity[severity];
               const prefix = `\n[${chalk.cyan('Validate HTML Offline')}] ${page} (${line}:${column})`;
               const selectorMsg = selector ? ` ${chalk.cyan(selector)}` : '';
+
               output += `${prefix}${selectorMsg}:\n${logOutput.underline(title)}: ${logOutput(message)}\n`;
             }
           });
@@ -86,11 +95,13 @@ const buildHTML = () => src(['source/**/*.html', '!source/**/_*.html'])
       } if (output) {
 				console.log(output);
 			}
+
       return tree;
     })()
   ]))
   .pipe(require('gulp-html-beautify')())
   .pipe(dest('.'));
+
 const buildCSS = () => src(['source/**/*.css', '!source/**/_*.css'])
   .pipe(require('gulp-postcss')([
     require('postcss-easy-import')(),
@@ -102,6 +113,7 @@ const buildCSS = () => src(['source/**/*.css', '!source/**/_*.css'])
     })
   ]))
   .pipe(dest('.'));
+
 const testBuildedCSS = () => src(['**/*.css', '!source/**/*.css', '!node_modules/**/*.css'])
   .pipe(require('gulp-postcss')([
     require('stylelint')({ fix: true }),
@@ -110,6 +122,7 @@ const testBuildedCSS = () => src(['**/*.css', '!source/**/*.css', '!node_modules
       throwError: false
     })
   ]));
+
 const optimizeImages = () => src('source/**/*.{svg,png,jpg}')
   .pipe(imagemin([
     imagemin.svgo({
@@ -157,22 +170,25 @@ const optimizeImages = () => src('source/**/*.{svg,png,jpg}')
   ]))
   .pipe(gulpIf(firstRun, dest('source')))
   .pipe(dest('.'));
+
 const reload = (done) => {
   server.reload();
   done();
 };
+
 const startServer = () => {
   firstRun = false;
+
   server.init({
     cors: true,
     server: '.',
     ui: false
   });
 
-  watch('source/**/*.html', series(buildHTML, reload));
   watch('source/**/*.{html,svg}', series(buildHTML, reload));
   watch('source/**/*.css', series(buildCSS, testBuildedCSS, reload));
   watch('source/**/*.{svg,png,jpg}', series(optimizeImages, reload));
 };
+
 exports.test = testBuildedCSS;
 exports.default = series(parallel(buildHTML, buildCSS, optimizeImages), testBuildedCSS, startServer);
